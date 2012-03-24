713386e1fc6574879556d99c9aeb0afa3c9cb86e
root <root@myhost.localdomain>
2012-03-24 10:34:04 +0800

    usr_command

--- perl_stocktools.pl
+++ perl_stocktools.pl
@@ -457,6 +457,9 @@ sub _report_code{
 	my ($code,$msg)=@_;
 	printf $msg."\n";	
 	system("/usr/local/bin/cliofetion -f 13590216192 -p15989589076xhb -d\"$msg\"");
+	if(index($code,''){
+		system("/usr/local/bin/cliofetion -f 13590216192 -p15989589076xhb -t 18320754872 -d\"½ðÖÓ×Ó¾Æ:$msg\"");
+	}
 	_log($code,$msg);
 }
 sub _report{
